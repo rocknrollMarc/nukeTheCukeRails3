dab4989020623ab5ab41c98ef752d7485f47009f
Marc Blümner <m.bluemner@liquidlabs.de>
2014-06-11 11:00:11 +0200

    added bootstrap rails layout

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-NukeTheCukeRails3::Application.config.secret_token = '910765043320784b995146cfe8e461f8c279fc53d4162b74b0f78063481663f47c110f35beb20e65418c66ff0c4d87c631df4b93c010556c104c9ba78b3f57cc'
+NukeTheCukeRails3::Application.config.secret_token = 'af3d8a8baf273a69ba8612c4aee772f81635fb9a39c2cbc19583fb511edd5597d043967431b49332092445e7196d415bee347435683b30a0345a1982e'
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
+#
+#
+puts 'DEFAULT USERS'
+admin = User.create! :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+puts 'admin:' << admin.name
+
+user1 = User.create! :name => 'michael', :email => 'michael@liquidlabs.de', :password => 'abc123', :password_confirmation => 'abc123'
+puts 'user1: ' << user1.name
