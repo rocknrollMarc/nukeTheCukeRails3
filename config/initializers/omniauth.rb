6158da71156c13388b01587b0fe9a46faee74277
rocknrollMarc <marc.bluemner@gmail.com>
2014-06-11 20:12:43 +0200

    deleted master branch is we areworking with gitFlow

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, ENV['OMNIAUTH_PROVIDER_KEY'], ENV['OMNIAUTH_PROVIDER_SECRET']
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-NukeTheCukeRails3::Application.config.secret_token = 'af3d8a8baf273a69ba8612c4aee772f81635fb9a39c2cbc19583fb511edd5597d043967431b49332092445e7196d415bee347435683b30a0345a1982e'
