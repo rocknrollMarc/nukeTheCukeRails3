ee4e24330e2a416a1d00022fbed5b98bf95eb368
rocknrollMarc <marc.bluemner@gmail.com>
2014-06-11 17:50:10 +0200

    gitflow

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -7,6 +7,8 @@ NukeTheCukeRails3::Application.configure do
   # and recreated between test runs.  Don't rely on the data there!
   config.cache_classes = true
 
+  #just a test for gitflow":w
+  #
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = "public, max-age=3600"
